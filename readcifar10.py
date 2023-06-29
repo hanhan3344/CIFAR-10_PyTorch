b8f3a43a901bb891e5b1891161c19651dd084473
hanhan3344 <915489043@qq.com>
2023-06-29 21:19:41 +0800

    修复readcifar10.py一次只能读取单个训练集或测试集的bug

--- readcifar10.py
+++ readcifar10.py
@@ -23,9 +23,13 @@ label_name = ["airplane",
               "truck"]
 
 train_list = glob.glob(
-    'cifar-10-batches-py/test_batch*')
-print(train_list)
-save_path = "TEST"
+    'cifar-10-batches-py/data_batch*')
+test_list = glob.glob(
+    'cifar-10-batches-py/test_batch*'
+)
+# print(train_list)
+train_save_path = "TRAIN"
+test_save_path = "TEST"
 
 for l in train_list:
     print(l)
@@ -46,8 +50,32 @@ for l in train_list:
         # cv2.imshow("im_data", cv2.resize(im_data, (256, 256)))
         # cv2.waitKey(0)
 
-        if not os.path.exists("{}/{}".format(save_path, im_label_name)):
-            os.makedirs("{}/{}".format(save_path, im_label_name))
+        if not os.path.exists("{}/{}".format(train_save_path, im_label_name)):
+            os.makedirs("{}/{}".format(train_save_path, im_label_name))
 
-        cv2.imwrite("{}/{}/{}".format(save_path, im_label_name,
+        cv2.imwrite("{}/{}/{}".format(train_save_path, im_label_name,
                     im_name.decode("utf-8")), im_data)
+for l in test_list:
+    print(l)
+    l_dict = unpickle(l)
+    print(l_dict.keys())
+
+    for im_idx, im_data in enumerate(l_dict[b'data']):
+        # print(im_idx)
+        # print(im_data)
+        im_label = l_dict[b'labels'][im_idx]
+        im_name = l_dict[b'filenames'][im_idx]
+        print(im_label, im_name, im_data)
+
+        im_label_name = label_name[im_label]
+        im_data = np.reshape(im_data, (3, 32, 32))
+        im_data = np.transpose(im_data, (1, 2, 0))
+
+        # cv2.imshow("im_data", cv2.resize(im_data, (256, 256)))
+        # cv2.waitKey(0)
+
+        if not os.path.exists("{}/{}".format(test_save_path, im_label_name)):
+            os.makedirs("{}/{}".format(test_save_path, im_label_name))
+
+        cv2.imwrite("{}/{}/{}".format(test_save_path, im_label_name,
+                                      im_name.decode("utf-8")), im_data)
